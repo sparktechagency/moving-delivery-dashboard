eeca917236bba54e90f5bc59dd8b3d7079609885
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2025-11-25 14:06:10 +0600

    BASE_URL update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://lopez-marble-tablet-airlines.trycloudflare.com"
+export const BASE_URL = "http://13.39.40.82:8001"
+// export const BASE_URL = "https://lopez-marble-tablet-airlines.trycloudflare.com"
 
 
 // "https://lopez-marble-tablet-airlines.trycloudflare.com"
