48480bebd0920759e7074a2a222c237c953cdba9
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2025-11-26 15:07:42 +0600

    u

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,20 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { router } from "./router/Routes";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./features/store";
-import { ConfigProvider,App as AntdApp } from "antd"; 
+import { ConfigProvider, App as AntdApp } from "antd";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <ConfigProvider>
-      <AntdApp>
-      <RouterProvider router={router} />
-      </AntdApp>
-        
-    </ConfigProvider>
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <ConfigProvider>
+        <AntdApp>
+          <RouterProvider router={router} />
+        </AntdApp>
+      </ConfigProvider>
+    </Provider>
+  </StrictMode>
 );
