816b230d8bbe68cee3f5145dc955667dd9e0e189
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2025-11-26 15:19:59 +0600

    update the vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: "8003",
-  // },
+  server: {
+    host: "0.0.0.0",
+    port: "8003",
+  },
   plugins: [react()],
 });
